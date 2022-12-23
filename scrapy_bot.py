4614aaed984890b2747faf46bd39a770f5a895d6
sadatyussuf <tommyleespartan0@gmail.com>
2022-12-23 23:30:13 +0000

    completed functionality to search for top movies and shows based on GENRE

--- scrapy_bot.py
+++ scrapy_bot.py
@@ -1,2 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
+
+
+GENRE_URL = "https://www.imdb.com/search/title/"
+
+
+def search_genre(genre):
+    page = requests.get(f"{GENRE_URL}?genres={genre}")
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = []
+
+    count = 1
+    for res in soup.find_all("div", {"class": "lister-item mode-advanced"}):
+        film_rating = (
+            res.find("div", {"class": "inline-block ratings-imdb-rating"})
+            .find("strong")
+            .get_text()
+        )
+
+        film_name = res.find("h3", {"class": "lister-item-header"}).find("a").get_text()
+
+        film_position = (
+            res.find("h3", {"class": "lister-item-header"})
+            .find("span", {"class": "lister-item-index unbold text-primary"})
+            .get_text()
+        )
+
+        print(film_position, film_name, film_rating)
+        results.append(
+            {
+                "film_position": film_position,
+                "film_name": film_name,
+                "film_rating": film_rating,
+            }
+        )
+
+        if count >= 10:
+            break
+        count += 1
+    print("--------------------------------")
+    print(results)
+    print("--------------------------------")
+
+    # print(soup.prettify())
+
+
+search_genre("action")
