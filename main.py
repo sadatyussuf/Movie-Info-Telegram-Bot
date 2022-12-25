0da2ceb6ae21ba60733ba3b5332fb6422b31d576
sadatyussuf <tommyleespartan0@gmail.com>
2022-12-25 09:23:49 +0000

    code for /genre command

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ from telegram.ext import (
     filters,
 )
 
+# from telegram.parsemode import ParseMode
+from telegram.constants import ParseMode
+
+from scrapy_bot import search_genre, search_movie
+
 load_dotenv()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
@@ -21,46 +26,53 @@ logging.basicConfig(
     format=" %(asctime)s - %(name)s - %(levelname)s: %(message)s", level=logging.INFO
 )
 
-# print(ACCESS_TOKEN)
+
 async def start(update: Update, context: ContextTypes):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot,Please talk to me"
+        chat_id=update.effective_chat.id,
+        text=" Hi! IMDb Info Dumper at your service. \n\n With me you can get Info about your favorite movies and series on IMDb. \n\n /genre *GENRE_NAME* : Fetches list of top 10 movies and tv shows belonging to that genre  \n\n /movie *MOVIE_NAME* : Fetches movie name with genre and rating of the movie",
+        parse_mode=ParseMode.MARKDOWN,
     )
 
 
-async def echo(update: Update, context: ContextTypes):
+async def genre(update: Update, context: ContextTypes):
+    if len(context.args) == 0:
+        text_genre_title = "Please add a genre name e.g.  /genre *comedy*"
+    if len(context.args) > 1:
+        text_genre_title = "Please add only one genre name e.g.  /genre *comedy*"
+
+    if len(context.args) == 1:
+        text_genre_title = ""
+        text = context.args[0].lower()
+        get_genre_info = search_genre(text)
+
+        if len(get_genre_info) > 0:
+            for line in get_genre_info:
+                text_genre_title += f"*{line['film_position']}* 🎥 {line['film_name']} - 🌟*{line['film_rating']}* \n\n"
+
+        # if results is empty
+        if len(get_genre_info) == 0:
+            text_genre_title = "No result Found"
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text
+        chat_id=update.effective_chat.id,
+        text=text_genre_title,
+        parse_mode=ParseMode.MARKDOWN,
     )
 
 
-async def caps(update: Update, context: ContextTypes):
-    text_caps = " ".join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-    # print("=====================================")
-    # print(context.args)
+def main():
+    application = ApplicationBuilder().token(ACCESS_TOKEN).build()
 
+    start_handler = CommandHandler("start", start)
+    genre_handler = CommandHandler("genre", genre)
 
-async def unknown(update: Update, context: ContextTypes):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command"
-    )
+    application.add_handler(start_handler)
+    application.add_handler(genre_handler)
+
+    application.run_polling()
 
 
 if __name__ == "__main__":
     # asyncio.run(main())
-    application = ApplicationBuilder().token(ACCESS_TOKEN).build()
-
-    start_handler = CommandHandler("start", start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    caps_handler = CommandHandler("caps", caps)
-
-    unknown_handler = MessageHandler(filters.COMMAND, unknown)
-
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-    application.add_handler(caps_handler)
-    application.add_handler(unknown_handler)
-
-    # application.run_polling(stop_signals=None)
-    application.run_polling()
+    main()
