d641bd6cae60ae6734d922507c623c75a0efe3ea
sadatyussuf <tommyleespartan0@gmail.com>
2022-12-28 21:18:11 +0000

    completed function when /movie command is  invoked

--- main.py
+++ main.py
@@ -37,22 +37,47 @@ async def start(update: Update, context: ContextTypes):
 
 async def genre(update: Update, context: ContextTypes):
     if len(context.args) == 0:
-        text_genre_title = "Please add a genre name e.g.  /genre *comedy*"
+        text_genre_title = (
+            "Please add a genre name attached to the command e.g.  /genre *comedy*"
+        )
     if len(context.args) > 1:
-        text_genre_title = "Please add only one genre name e.g.  /genre *comedy*"
+        text_genre_title = "Please add only one genre name attached to the command e.g.  /genre *comedy*"
 
     if len(context.args) == 1:
-        text_genre_title = ""
+        text_genre_title = f" **_🎥 Top 10 Popular {context.args[0].upper()} Movies and TV Shows on IMDb_** \n\n"
         text = context.args[0].lower()
         get_genre_info = search_genre(text)
 
         if len(get_genre_info) > 0:
             for line in get_genre_info:
-                text_genre_title += f"*{line['film_position']}* 🎥 {line['film_name']} - 🌟*{line['film_rating']}* \n\n"
+                text_genre_title += f"*{line['film_position']}*  {line['film_name']} - 🌟*{line['film_rating']}* \n\n"
 
         # if results is empty
         if len(get_genre_info) == 0:
             text_genre_title = "No result Found"
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text_genre_title,
+        parse_mode=ParseMode.MARKDOWN,
+    )
+
+
+async def movie(update: Update, context: ContextTypes):
+
+    text_genre_title = (
+        "Please add a genre name attached to the command e.g.  /genre *Men In Black*"
+    )
+    # print(context.args)
+    if len(context.args) > 0:
+        text = " ".join(context.args)
+        get_movie_info = search_movie(text)
+
+        text_genre_title = (
+            f"**_ No Result Found for Movie with title_** *{text.upper()}* "
+        )
+        if len(get_movie_info) > 0:
+            text_genre_title = f" {get_movie_info[0]['film_name']} - 🌟 *{get_movie_info[0]['film_rating']}* 🌟  **_{get_movie_info[0]['film_genre']}_** \n\n"
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -66,9 +91,11 @@ def main():
 
     start_handler = CommandHandler("start", start)
     genre_handler = CommandHandler("genre", genre)
+    movie_handler = CommandHandler("movie", movie)
 
     application.add_handler(start_handler)
     application.add_handler(genre_handler)
+    application.add_handler(movie_handler)
 
     application.run_polling()
 
